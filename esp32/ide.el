74eb17b036612c9b6b184a7d1f2948a66436be37
Heedong Kim <klenui@gmail.com>
2020-04-02 22:15:09 +0900

    build work

--- esp32/ide.el
+++ esp32/ide.el
@@ -1,12 +1,12 @@
-;; This buffer is for text that is not saved, and for Lisp evaluation.
-;; To create a file, visit it with C-x C-f and enter text in its buffer.
-
 (defun build()
   (interactive)
-  (let ((old-pwd (eshell/pwd)))
+  (let ((old-pwd (eshell/pwd))
+	(old-buf (current-buffer)))
     (cd "~/kameleon/esp32")
     (compile "idf.py build")
+    (set-buffer "*compilation*")
+    (goto-char (point-max))
+    (set-buffer old-buf)
     (cd old-pwd)))
-
 
 	
---
+++ esp32/main/targets/boards/esp32-wrover-b/target.cmake
@@ -0,0 +1,15 @@
+set(TARGET_SOURCE
+  ${CMAKE_CURRENT_LIST_DIR}/src/adc.c
+  ${CMAKE_CURRENT_LIST_DIR}/src/flash.c
+  ${CMAKE_CURRENT_LIST_DIR}/src/gpio.c
+  ${CMAKE_CURRENT_LIST_DIR}/src/i2c.c
+  ${CMAKE_CURRENT_LIST_DIR}/src/pwm.c
+  ${CMAKE_CURRENT_LIST_DIR}/src/spi.c
+  ${CMAKE_CURRENT_LIST_DIR}/src/storage.c
+  ${CMAKE_CURRENT_LIST_DIR}/src/system.c
+  ${CMAKE_CURRENT_LIST_DIR}/src/tty.c
+  ${CMAKE_CURRENT_LIST_DIR}/src/uart.c)
+
+set(TARGET_INCLUDE
+  ${CMAKE_CURRENT_LIST_DIR}/include)
+
