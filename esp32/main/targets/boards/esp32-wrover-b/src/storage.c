25319e67a9eca46775bd76ca9a16430cd333bbf4
Heedong Kim <klenui@gmail.com>
2020-07-06 00:55:47 +0900

    nvs storage

--- esp32/main/targets/boards/esp32-wrover-b/src/storage.c
+++ esp32/main/targets/boards/esp32-wrover-b/src/storage.c
@@ -19,36 +19,157 @@
  * SOFTWARE.
  */
 
+
+#include <string.h>
 #include "kameleon_core.h"
 #include "storage.h"
+#include <freertos/FreeRTOS.h>
+#include <nvs_flash.h>
+#include <nvs.h>
+
+#define STORAGE_NAMESPACE ("storage")
 
 int storage_clear()
 {
+  nvs_handle_t h;
+  esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &h);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) opening NVS handle\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+  err = nvs_erase_all(h);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) nvs_erase_all\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+
+  err = nvs_commit(h);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) nvs_commit\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+
+  nvs_close(h);  
   return 0;
 }
 
 int storage_length()
 {
-  return 0;
+  nvs_handle_t h;
+  esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READONLY, &h);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) opening NVS handle\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+
+  size_t used;
+  err = nvs_get_used_entry_count(h, &used);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) nvs_get_used_entry_count\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+
+  nvs_close(h);
+  return used;
 }
 
 int storage_get_item(const char *key, char *buf)
 {
+  nvs_handle_t h;
+  esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READONLY, &h);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) opening NVS handle\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+
+  size_t size;
+  err = nvs_get_str(h, key, buf, &size);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) nvs_get_str\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+
+  nvs_close(h);
   return 0;
 }
 
 int storage_set_item(const char *key, char *buf)
 {
+  nvs_handle_t h;
+  esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &h);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) opening NVS handle\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+
+  err = nvs_set_str(h, key, buf);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) nvs_set_str\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+
+  err = nvs_commit(h);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) nvs_commit\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+
+  nvs_close(h);  
   return 0;
 }
 
 int storage_remove_item(const char *key)
 {
+  nvs_handle_t h;
+  esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &h);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) opening NVS handle\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+  err = nvs_erase_key(h, key);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) nvs_erase_key\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+
+  err = nvs_commit(h);
+  if ( err != ESP_OK ) {
+    printf("ERROR (%s) nvs_commit\n", esp_err_to_name(err));
+    nvs_close(h);
+    return -1;
+  }
+
+  nvs_close(h);  
   return 0;
 }
 
 int storage_key(const int index, char *buf)
 {
+  nvs_iterator_t it = nvs_entry_find("nvs", STORAGE_NAMESPACE, NVS_TYPE_ANY);
+  for ( int i = 0 ; it != NULL && i < index ; i++ ) it= nvs_entry_next(it);
+
+  if ( it == NULL ) {
+    nvs_release_iterator(it);
+    return -1;
+  }
+
+  nvs_entry_info_t info;
+  nvs_entry_info(it, &info);
+  strcpy(buf, info.key);
+
+  nvs_release_iterator(it);
   return 0;
 }
 
