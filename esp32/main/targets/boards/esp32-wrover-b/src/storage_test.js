9b1127852444a34643504cce4fd462870c5e9f83
Heedong Kim <klenui@gmail.com>
2020-07-16 18:16:06 +0900

    storage test code

--- esp32/main/targets/boards/esp32-wrover-b/src/storage_test.js
+++ esp32/main/targets/boards/esp32-wrover-b/src/storage_test.js
@@ -1,8 +1,9 @@
-const Storage=require('storage');
-const storage = new Storage();
+print("in script ok\n");
+var lib=require('storage');
+var storage = new lib.Storage();
 storage.setItem('value1', 'abcd');
 storage.setItem('value2', '1234');
-const val1 = storage.getItem('value1');
-const val2 = storage.getItem('value2');
-console.log(`val1 : ${val1}`);
-console.log(`val2 : ${val2}`);
+var val1 = storage.getItem('value1');
+var val2 = storage.getItem('value2');
+console.log('val1 : '+val1);
+console.log('val2 : '+val2);
